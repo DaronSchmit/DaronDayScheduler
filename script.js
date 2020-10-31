60eef69fd31ddf9bd67b28f17f748720c415232b
DaronS23 <darons23@gmail.com>
2020-10-31 15:07:41 -0500

    added comments to script.js

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
+
+//get date and time information from moment.js
+//this can be manipulated by changing local machine date/time
 let today = moment().format("dddd, MMMM Do YYYY");
 let time = moment().hour();
 
-$("#currentDay").text(today);
+$("#currentDay").text(today); //update date header
 
-// for(let i=9; i < 17; i++){
-//     let hour = i +"am";
-//     if(parseInt(hour) > 12){
-//         hour = parseInt(hour)-12;
-//         hour = hour + "pm";
-//     }
-//     let newHour = $("<row class='"+hour+"'></row>");
-//     newHour.append("<div class='col-sm-2'>"+hour+"</div><div class='col-sm-8'>events go here</div><div class='col-sm-2 save-button'><i class='fas fa-save'></i></div>");
 
-//     $(".container").append(newHour);
-// }
-
+//iterate through hour divs, each with a class that is just a number (probably not best practice)
+//add a css class to change color accordingly
 for(let i = 9; i <= 17; i++){
     if(time < i){
         $("."+i).addClass("future");
@@ -27,25 +21,28 @@ for(let i = 9; i <= 17; i++){
     }
 }
 
+
+//get any info from localStorage
 let scheduledEvents = JSON.parse(localStorage.getItem('events'));
 if(scheduledEvents === null){
-   scheduledEvents = ["","","","","","","","",""];
+   scheduledEvents = ["","","","","","","","",""]; //if localstorage has no events, make an empty array to be manipulated later
 }
 else{
-    for(let i = 9; i <=17; i++){
+    for(let i = 9; i <=17; i++){ //if localstorage has events in it, update placeholders
         $("#form-"+i).attr('placeholder', scheduledEvents[i-9]);
     }
 }
 
-let input;
-
+let input; //insantiate input for later use
+//make all the save-button con click events
 $("#9am-save-button").on('click', function(){
-    input = $("#form-9").val();
-    $("#form-9").attr("placeholder", input);
-    scheduledEvents[0] = input;
-    localStorage.setItem('events', JSON.stringify(scheduledEvents));
+    input = $("#form-9").val(); //get input from webpage
+    $("#form-9").attr("placeholder", input); //make placeholder the saved text
+    scheduledEvents[0] = input; //add event info to scheduledEvents variable
+    localStorage.setItem('events', JSON.stringify(scheduledEvents)); //update localStorage
 });
-
+ //repeat for 10am-5pm
+ //You could make this a single onclick for all the buttons and pull more information from the buttons, but this works and I'm not about to change it. we strive to be DRY but sometimes we fall short
 $("#10am-save-button").on('click', function(){
     input = $("#form-10").val();
     $("#form-10").attr("placeholder", input);
